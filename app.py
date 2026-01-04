2f59bf391fa6cd0dc5504506b9c86f872c95ea3f
Shanmugapriyan <71700679+ShanmugapriyanG@users.noreply.github.com>
2026-01-03 19:23:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import joblib
 import pandas as pd
-from pydantic import Field
 
 # --------------------------------------------------
 # FastAPI app
@@ -37,51 +35,53 @@ class EmployeeData(BaseModel):
     class Config:
         allow_population_by_field_name = True
 
-# --------------------------------------------------
-# Prediction Endpoint
-# --------------------------------------------------
 
+# --------------------------------------------------
+# Routes
+# --------------------------------------------------
 @app.get("/")
 def read_root():
-  return {"Welcome for the Hackathon - Employee Promotion Prediction"}
+    return {"message": "Employee Promotion Prediction API is running"}
+
 
 @app.post("/predict")
 def predict_promotion(data: EmployeeData):
-    # Convert to DataFrame
+
+  
     x_input = pd.DataFrame([data.dict(by_alias=True)])
 
-    # ------------------------------
-    # Feature Engineering
-    # ------------------------------
+    # --------------------------------------------------
+    # Feature Engineering (IDENTICAL to training)
+    # --------------------------------------------------
     x_input['training_efficiency'] = (
         x_input['avg_training_score'] / (x_input['length_of_service'] + 1)
     )
 
     x_input['performance_index'] = (
         x_input['avg_training_score']
-        * (x_input['KPIs_met_80'] + 1)
-        * (x_input['awards_won'] + 1)
+        * (x_input['KPIs_met >80%'] + 1)
+        * (x_input['awards_won?'] + 1)
     )
 
     x_input['kpi_per_training'] = (
-        x_input['KPIs_met_80'] / (x_input['no_of_trainings'] + 1)
+        x_input['KPIs_met >80%'] / (x_input['no_of_trainings'] + 1)
     )
 
     x_input['awards_per_service'] = (
-        x_input['awards_won'] / (x_input['length_of_service'] + 1)
+        x_input['awards_won?'] / (x_input['length_of_service'] + 1)
     )
 
     x_input['score_times_awards'] = (
-        x_input['avg_training_score'] * (x_input['awards_won'] + 1)
+        x_input['avg_training_score'] * (x_input['awards_won?'] + 1)
     )
 
     x_input['score_times_kpi'] = (
-        x_input['avg_training_score'] * (x_input['KPIs_met_80'] + 1)
+        x_input['avg_training_score'] * (x_input['KPIs_met >80%'] + 1)
     )
 
-    # ------------------------------
+    # --------------------------------------------------
     # Prediction
-    # ------------------------------
+    # --------------------------------------------------
     prob = model.predict_proba(x_input)[0, 1]
     prediction = int(prob >= THRESHOLD)
 
